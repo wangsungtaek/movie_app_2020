70746f839aa5c4ae1e03cf0b6744a0f2ca19b376
Taek <68735491+wangsungtaek@users.noreply.github.com>
2020-11-22 11:03:05 -0400

    201122

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({name, picture}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src = {picture} alt = {name}/>
+    </div>
+  )
+}
+
+const foodILike = [
+  {
+    id : 1,
+    name : 'Kimchi',
+    image : 'https://www.dataiku.com/wp-content/uploads/2019/10/chris-ried-ieic5Tq8YMk-unsplash-1618x1080.jpg',
+  },
+  {
+    id : 2,
+    name : 'samgyeopsal',
+    image : 'https://www.dataiku.com/wp-content/uploads/2019/10/chris-ried-ieic5Tq8YMk-unsplash-1618x1080.jpg',
+  },
+  {
+    id : 3,
+    name : 'Bibimbap',
+    image : 'https://www.dataiku.com/wp-content/uploads/2019/10/chris-ried-ieic5Tq8YMk-unsplash-1618x1080.jpg',
+  },
+  {
+    id : 4,
+    name : 'Doncasu',
+    image : 'https://www.dataiku.com/wp-content/uploads/2019/10/chris-ried-ieic5Tq8YMk-unsplash-1618x1080.jpg',
+  },
+  {
+    id : 5,
+    name : 'Kimbap',
+    image : 'https://www.dataiku.com/wp-content/uploads/2019/10/chris-ried-ieic5Tq8YMk-unsplash-1618x1080.jpg',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => 
+        (<Food key = {dish.id} name = {dish.namae} picture = {dish.image}/>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
